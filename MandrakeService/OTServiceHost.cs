da6716ec55551d44d9e069ac2510ddd2f4f15ca3
Peter Szucs <pszucs@outlook.com>
2015-01-21 17:53:30 +0100

    Minor modifications around service and standalone
    App.config is removed from MandrakeService, instead sample config files
    can be found there for different hosting scenarios (web.sample.config
    and app.sample.config)

--- MandrakeService/OTServiceHost.cs
+++ MandrakeService/OTServiceHost.cs
@@ -15,6 +15,7 @@ namespace Mandrake.Service
         public OTServiceHost(Type t, params Uri[] baseAddresses): base(t, baseAddresses)
         {
             this.AddGenericResolver();
+            Console.WriteLine("OTServiceHost CREATED");
         }
     }
 
@@ -22,6 +23,7 @@ namespace Mandrake.Service
     {
         protected override ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses)
         {
+            Console.WriteLine("OTServiceHostFactory CALLED");
             return new OTServiceHost(serviceType, baseAddresses);
         }
     }
